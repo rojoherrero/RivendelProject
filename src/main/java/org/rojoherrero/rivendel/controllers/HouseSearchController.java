0d27f0931b91762c7043c21f77f9e488bb415f85
rojoherrero <rojoherrero@gmail.com>
2016-07-13 10:45:22 +0200

    Creating DB entries achieve

--- src/main/java/org/rojoherrero/rivendel/controllers/HouseSearchController.java
+++ src/main/java/org/rojoherrero/rivendel/controllers/HouseSearchController.java
@@ -1,10 +1,6 @@
 package org.rojoherrero.rivendel.controllers;
 
-import javax.validation.Valid;
-
-import org.rojoherrero.rivendel.models.HouseRegistrationForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
