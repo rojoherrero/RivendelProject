93423b818f60b98d8b63694a5623323cffb42a48
rojoherrero <rojoherrero@gmail.com>
2016-07-19 20:36:24 +0200

    House deletion achieve

--- src/main/java/org/rojoherrero/rivendel/controllers/HouseModifyController.java
+++ src/main/java/org/rojoherrero/rivendel/controllers/HouseModifyController.java
@@ -1,6 +1,9 @@
 package org.rojoherrero.rivendel.controllers;
 
+import java.util.Calendar;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.rojoherrero.rivendel.models.House;
 import org.rojoherrero.rivendel.models.HouseModifyForm;
@@ -8,6 +11,7 @@ import org.rojoherrero.rivendel.repositories.HouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +23,9 @@ public class HouseModifyController {
 	@Autowired
 	private HouseRepository houseRepository;
 
+	private String errorTrace;
 	private House houseToModify;
+	private Long houseId;
 
 	/**
 	 * 
@@ -29,17 +35,56 @@ public class HouseModifyController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String housesList(Model model) {
 
-		List<House> allHouses = houseRepository.findAll();
-		model.addAttribute("allHouses", allHouses);
-
-		return ("modify/houses_list");
+		try {
+			List<House> allHouses = houseRepository.findAll();
+			model.addAttribute("allHouses", allHouses);
+			return ("modify/houses_list");
+		} catch (Exception e) {
+			e.printStackTrace();
+			errorTrace = e.toString();
+			return ("redirect:/house_modify/error");
+		}
 	}
 
-	@RequestMapping("/house_to_modify/houseId/{houseId}")
-	public String modifyHouse(@PathVariable("houseId") Long houseId, HouseModifyForm modifyForm, Model model) {
-
-		houseToModify = houseRepository.findOne(houseId);
+	@RequestMapping(value = "/house_to_modify/houseId/{houseId}", method = RequestMethod.GET)
+	public String modifyHouse(@PathVariable("houseId") Long _houseId, HouseModifyForm modifyForm, Model model) {
+		houseToModify = houseRepository.findOne(_houseId);
 		model.addAttribute("house", houseToModify);
 		return ("modify/modify_form");
+	}
+
+	@RequestMapping(value = "/house_to_delete/houseId/{houseId}")
+	public String deleteHouse(@PathVariable("houseId") Long _houseId) {
+		houseRepository.delete(_houseId);
+		return ("modify/delete_success");
+	}
+
+	@RequestMapping(value = "/update_house", method = RequestMethod.POST)
+	public String postModifyHouse(@Valid HouseModifyForm modifyForm, BindingResult result, Model model) {
+
+		if (result.hasErrors()) {
+			return ("modify/modify_form");
+		}
+
+		try {
+			houseRepository.upDateHouseData(modifyForm.getQuarter(), modifyForm.getStreetName(),
+					modifyForm.getStreetNumber(), modifyForm.getZipCode(), modifyForm.getTown(),
+					modifyForm.getCountry(), modifyForm.getHouseSurface(), modifyForm.getGardenSurface(),
+					modifyForm.getHouseSurface() + modifyForm.getGardenSurface(), Calendar.getInstance(),
+					houseToModify.getId());
+			return ("redirect:/index");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			errorTrace = e.toString();
+			return ("house_modify/error");
+		}
+
+	}
+
+	@RequestMapping(value = "/house_modify/error")
+	public String errorPage(Model model) {
+		model.addAttribute("error", errorTrace);
+		return ("modify/error");
 	}
 }
--- src/main/java/org/rojoherrero/rivendel/repositories/HouseRepository.java
+++ src/main/java/org/rojoherrero/rivendel/repositories/HouseRepository.java
@@ -1,9 +1,11 @@
 package org.rojoherrero.rivendel.repositories;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.rojoherrero.rivendel.models.House;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -16,7 +18,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface HouseRepository extends CrudRepository<House, Long> {
 
 	/**
-	 * Retrieve all the hauses
+	 * Retrieve all the houses
 	 * 
 	 * @return List<House>
 	 * @author rojoherrero
@@ -40,6 +42,32 @@ public interface HouseRepository extends CrudRepository<House, Long> {
 	 */
 	@Query("SELECT houseSurface FROM House h")
 	List<Double> findHouseSurfaces();
+
+	/**
+	 * Update the data for houses entities
+	 * 
+	 * @param quarter
+	 * @param streetName
+	 * @param streetNumber
+	 * @param zipCode
+	 * @param town
+	 * @param country
+	 * @param houseSurface
+	 * @param gardenSurface
+	 * @param totalSurface
+	 * @param modificationDate
+	 * @param id
+	 * @author rojoherrero
+	 */
+	@Modifying
+	@Query("UPDATE House h SET h.quarter = ?1, h.streetName = ?2, h.streetNumber = ?3, "
+			+ "h.zipCode = ?4, h.town = ?5, h.country = ?6, h.houseSurface = ?7,"
+			+ "h.gardenSurface = ?8, h.totalSurface = ?9, h.modificationDate = ?10 WHERE h.id = ?11")
+	void upDateHouseData(String quarter/* 1 */, String streetName/* 2 */, Integer streetNumber/* 3 */,
+			Integer zipCode/* 4 */, String town /* 5 */, String country /* 6 */, Double houseSurface/* 7 */,
+			Double gardenSurface/* 8 */, Double totalSurface/* 9 */, Calendar modificationDate/* 10 */,
+			Long id/* 11 */);
+	/* Numbers after parameters are to help me to build the update query */
 
 	/**
 	 * Retrieve all the house with the ZipCode zipCode
