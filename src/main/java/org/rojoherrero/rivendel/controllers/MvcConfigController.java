15b779df3f87460cc1de4afbfefda090c8704906
rojoherrero <rojoherrero@gmail.com>
2016-08-27 16:45:24 +0200

    Refactor Controllers and enable Tomcat as server

---
+++ src/main/java/org/rojoherrero/rivendel/controllers/MvcConfigController.java
@@ -0,0 +1,17 @@
+package org.rojoherrero.rivendel.controllers;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class MvcConfigController extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/info_and_contact/about_us").setViewName("infoAndContact/aboutus");
+		registry.addViewController("/info_and_contact/contact_us").setViewName("infoAndContact/contactus");
+	}
+
+	
+}
