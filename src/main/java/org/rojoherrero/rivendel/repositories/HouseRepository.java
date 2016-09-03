f8dcdeee516d45636771e24247fed72d4cadebef
rojoherrero <rojoherrero@gmail.com>
2016-09-03 16:53:41 +0200

    House Repo changed from JpaRepository to CrudRepository

--- src/main/java/org/rojoherrero/rivendel/repositories/HouseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.rojoherrero.rivendel.repositories;
-
-import org.rojoherrero.rivendel.entities.House;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HouseRepository extends JpaRepository<House, Long> {
-
-}
